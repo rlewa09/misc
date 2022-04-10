e60977448530e78280b2d25dd3aafe399e60c0a5
rlewa09 <praveenvamsi8874@gmail.com>
2022-04-10 17:39:40 +0530

    java boiler codes

---
+++ java/boiler_int.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class boiler_int {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = 0;
+        t = scn.nextInt();
+
+        for(int p=0; p<t; p++) {
+//            int n;
+//            n = scn.nextInt();
+            solve();
+        }
+    }
+
+    private static void solve() {
+    }
+}
---
+++ java/boiler_str.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class boiler_str {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = 0;
+        t = scn.nextInt();
+
+        for(int p=0; p<t; p++) {
+//            int n;
+//            String str;
+//            n = scn.nextInt();
+//            str = scn.next();
+            solve();
+        }
+    }
+
+    private static void solve() {
+//        System.out.println(a);
+
+        return;
+    }
+}
